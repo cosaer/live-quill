52582d9c02164b7b97b96f237348cd0c71c1695f
Carlos Cesar Reyes Penagos <cesar.reyes@agrovizion.com.mx>
2024-06-17 14:26:12 -0700

    feat!: Add configuration file for CDNs and dark mode.

--- config/config.php
+++ config/config.php
@@ -4,5 +4,10 @@
  * You can place your custom package configuration in here.
  */
 return [
-
+	'pacakge_source' => [
+		'css' => 'https://cdn.jsdelivr.net/npm/quill@2/dist/quill.snow.css',
+		'js' => 'https://cdn.jsdelivr.net/npm/quill@2/dist/quill.js'
+	],
+	'dark_widget' => '',
+	'session_darkmode_key' => ''
 ];
