1ddc3412c93b90601d2302a95dd067a747a89e30
Carlos Cesar Reyes Penagos <cesar.reyes@agrovizion.com.mx>
2024-08-29 08:57:05 -0700

    perf(Dependencies): :arrow_up: Upgrade de livewire a versión 3 y paso  de paquete a versión beta.
    Updated the dependency to livewire version 3, and removed obsolete methods.

--- resources/views/live-quill.blade.php
+++ resources/views/live-quill.blade.php
@@ -4,57 +4,70 @@
 </div>
 
 @push('css')
-		<!-- Include stylesheet -->
-		<link href="{{ config('live-quill.pacakge_source.css') }}" rel="stylesheet"/>
-		@liveQuillDark
+  <!-- Include stylesheet -->
+  <link href="{{ config('live-quill.pacakge_source.css') }}" rel="stylesheet" />
+  @liveQuillDark
 @endpush
 @push('js')
-	<!-- Include the Quill library -->
-	<script src="{{ config('live-quill.pacakge_source.js') }}"></script>
+  <!-- Include the Quill library -->
+  <script src="{{ config('live-quill.pacakge_source.js') }}"></script>
   <script>
-		<!-- Initialize Quill editor -->
-    const quill = new Quill('#{{ $quillId }}', {
-      modules: {
-        toolbar: [
-          [{
-            header: [1, 2, false]
-          }],
-          ['bold', 'italic'],
-          ['blockquote', {
-            list: 'ordered'
-          }, {
-            list: 'bullet'
-          }],
-        ],
-      },
-      theme: "{{ config('live-quill.theme') }}",
+    // Function to switch between dark and light mode
+    function toggleDarkMode() {
+      var quillContainer = document.querySelector('.ql-container');
+      var quillToolbar = document.querySelector('.ql-toolbar');
+      quillContainer.classList.toggle('ql-dark-mode');
+      quillToolbar.classList.toggle('ql-dark-mode');
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+      document.addEventListener('livewire:init', () => {
+
+        const quillconf = {
+          modules: {
+            toolbar: [
+              [{
+                header: [1, 2, false]
+              }],
+              ['bold', 'italic'],
+              ['blockquote', {
+                list: 'ordered'
+              }, {
+                list: 'bullet'
+              }],
+            ],
+          },
+          theme: "{{ config('live-quill.theme') }}",
+        };
+        Livewire.hook('component.init', ({
+          component,
+          cleanup
+        }) => {
+          if (component.name === 'components.quill') {
+            const quill = new Quill('#{{ $quillId }}', quillconf);
+            quill.on('text-change', function() {
+              let val = document.getElementsByClassName('ql-editor')[0].innerHTML;
+              @this.value = val;
+            });
+            document.getElementsByClassName('ql-editor')[0].innerHTML = '{!! $value !!}';
+
+
+
+            if (@json(session(config('live-quill.session_darkmode_key', false)))) {
+              toggleDarkMode();
+            }
+          }
+        });
+
+
+        let darkMode = @json(config('live-quill.dark_widget', null));
+        if (darkMode != 'null' && darkMode) {
+          let darkWidget = document.querySelector("{{ config('live-quill.dark_widget') }}");
+          darkWidget.addEventListener('click', (e) => {
+            toggleDarkMode();
+          });
+        }
+      });
     });
-    quill.on('text-change', function() {
-      let val = document.getElementsByClassName('ql-editor')[0].innerHTML;
-      @this.set('value', val);
-    });
-
-		// Function to switch between dark and light mode
-		function toggleDarkMode() {
-			var quillContainer = document.querySelector('.ql-container');
-			var quillToolbar = document.querySelector('.ql-toolbar');
-			quillContainer.classList.toggle('ql-dark-mode');
-			quillToolbar.classList.toggle('ql-dark-mode');
-		}
-
-		document.addEventListener("DOMContentLoaded", () => {
-			if(@json(session(config('live-quill.session_darkmode_key',false))))
-			{
-				toggleDarkMode();
-			}
-			let darkMode = @json(config('live-quill.dark_widget', null));
-			if( darkMode != 'null' && darkMode){
-				let darkWidget = document.querySelector("{{ config('live-quill.dark_widget') }}");
-				darkWidget.addEventListener('click', (e) => {
-					toggleDarkMode();
-				});
-			}
-		});
-
   </script>
 @endpush
